e5dadc7fd37244fb6866e92c374e9f6d5ab14c99
Kushagra Singh <kushagrasingh772@gmail.com>
2020-05-26 00:10:44 +0530

    Added question.dart and brain.dart file
    A quizzler which will definitely check your general Knowledge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'quizBrain.dart';
+Quizbrain quizbrain =Quizbrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +26,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+  void checkAnswer(bool userPickedAnswer){
+    bool correct = quizbrain.getCorrectAnswer();
+    setState(() {
+    if(userPickedAnswer==correct){
+      scorekeeper.add(
+        Icon(Icons.check,color: Colors.green,),
+      );
+    }
+    else{
+      scorekeeper.add(Icon(
+        Icons.close,color: Colors.red,
+      ),
+      );
+    }
+      quizbrain.nextQuestion();
+    });
+  }
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizbrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +82,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -80,18 +101,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        )
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
